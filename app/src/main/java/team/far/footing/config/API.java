7e01b144dd77434bc216a5eec4858480d1a28c18
Luoyy <1070753575@qq.com>
2015-08-08 11:37:40 +0800

    添加了BaiDu SDK

--- app/src/main/java/team/far/footing/config/API.java
+++ app/src/main/java/team/far/footing/config/API.java
@@ -10,7 +10,8 @@ public class API {
     }
 
     public static class KEY {
-
+        public static String BmobApplicationID = "6a357dba207635a13f727ef9a5f6e1c5";
+        public static String BaiDuApplicationID = "7gPn7kClMxHnWE8DYHhlvpb9";
     }
 
 }
