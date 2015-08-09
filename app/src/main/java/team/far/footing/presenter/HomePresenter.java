b91461fdec2b7b5305a742bbad8cdac2c34dbcd8
moiling <super8moi@gmail.com>
2015-08-09 15:38:14 +0800

    添加launch界面，实现自动登录和切换用户，不知道有没有直接的方法判定是否存在用户，我先用了sp，之后能简化就简化吧

--- app/src/main/java/team/far/footing/presenter/HomePresenter.java
+++ app/src/main/java/team/far/footing/presenter/HomePresenter.java
@@ -3,11 +3,13 @@ package team.far.footing.presenter;
 import android.content.Context;
 import android.content.Intent;
 
+import team.far.footing.app.APP;
 import team.far.footing.model.bean.Userbean;
 import team.far.footing.ui.activity.WalkActivity;
 import team.far.footing.ui.vu.IHomeVu;
 import team.far.footing.util.BmobUtils;
 import team.far.footing.util.LogUtils;
+import team.far.footing.util.SPUtils;
 
 /**
  * Created by moi on 2015/8/9.
@@ -18,6 +20,8 @@ public class HomePresenter {
 
     public HomePresenter(IHomeVu v) {
         this.v = v;
+        // 当homePresenter被实例化，用sp保存登录状态
+        SPUtils.put(APP.getContext(), "isLogin", Boolean.TRUE);
         userbean = BmobUtils.getCurrentUser();
         LogUtils.d(userbean.getUsername());
         showUserInformation();
---
+++ app/src/main/java/team/far/footing/ui/activity/LaunchActivity.java
@@ -0,0 +1,50 @@
+package team.far.footing.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import team.far.footing.R;
+import team.far.footing.app.BaseActivity;
+import team.far.footing.util.SPUtils;
+
+public class LaunchActivity extends BaseActivity {
+
+    private Boolean isLogin = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_launch);
+        setBarTintColor(getResources().getColor(R.color.primary_color));
+        // 每次进入显示LOGO的地方先判断是否登陆过
+        isLogin = (Boolean) SPUtils.get(this, "isLogin", Boolean.FALSE);
+        myIntent();
+    }
+
+    private void myIntent() {
+        if (isLogin) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LaunchActivity.this, HomeActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                    LaunchActivity.this.startActivity(intent);
+                    LaunchActivity.this.finish();
+                }
+            }, 2000);
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LaunchActivity.this, LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                    LaunchActivity.this.startActivity(intent);
+                    LaunchActivity.this.finish();
+                }
+            }, 2000);
+        }
+    }
+}
