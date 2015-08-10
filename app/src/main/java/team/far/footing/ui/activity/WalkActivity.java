282f28d66eaebe9cd015b36bc18f6b396d5e145a
moiling <super8moi@gmail.com>
2015-08-10 15:44:01 +0800

    将地图定位和位置数据从view转移到了presenter里

--- app/src/main/java/team/far/footing/ui/activity/WalkActivity.java
+++ app/src/main/java/team/far/footing/ui/activity/WalkActivity.java
@@ -4,15 +4,10 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.baidu.location.BDLocation;
-import com.baidu.location.BDLocationListener;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.map.PolylineOptions;
 import com.baidu.mapapi.model.LatLng;
@@ -22,21 +17,17 @@ import java.util.ArrayList;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import team.far.footing.R;
-import team.far.footing.app.APP;
 import team.far.footing.app.BaseActivity;
+import team.far.footing.presenter.WalkPresenter;
+import team.far.footing.ui.vu.IWalkVu;
 
-public class WalkActivity extends BaseActivity {
+public class WalkActivity extends BaseActivity implements IWalkVu {
 
     @InjectView(R.id.toolbar) Toolbar mToolbar;
     @InjectView(R.id.map_walk) MapView mMapView;
     private BaiduMap mBaiduMap;
 
-    // 定位
-    private LocationClient mLocationClient;
-    private MyLocationListener mLocationListener;
-    private boolean isFirstIn = true;
-    // 存放绘制路线的端点
-    private ArrayList<LatLng> latLngs = new ArrayList<>();
+    private WalkPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,24 +36,21 @@ public class WalkActivity extends BaseActivity {
         ButterKnife.inject(this);
         initToolbar();
         initMap();
-        // 定位
-        initLocation();
+        presenter = new WalkPresenter(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // 开始定位
-        mBaiduMap.setMyLocationEnabled(true);
-        mLocationClient.start();
+        presenter.startLocation();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // 停止定位
-        mBaiduMap.setMyLocationEnabled(false);
-        mLocationClient.stop();
+        //停止定位
+        presenter.stopLocation();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
     }
@@ -79,25 +67,6 @@ public class WalkActivity extends BaseActivity {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
-    }
-
-    private void initLocation() {
-        mLocationClient = new LocationClient(APP.getContext());
-        mLocationListener = new MyLocationListener();
-        mLocationClient.registerLocationListener(mLocationListener);
-
-        LocationClientOption option = new LocationClientOption();
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
-
-        option.setCoorType("bd09ll");
-        option.setIsNeedAddress(true);
-        option.setOpenGps(true);
-        option.setLocationNotify(true);
-        // stop时不杀死service
-        option.setIgnoreKillProcess(true);
-        // 1s一次
-        option.setScanSpan(1000);
-        mLocationClient.setLocOption(option);
     }
 
     private void initMap() {
@@ -125,32 +94,25 @@ public class WalkActivity extends BaseActivity {
         });
     }
 
-    private class MyLocationListener implements BDLocationListener {
-        @Override
-        public void onReceiveLocation(BDLocation bdLocation) {
-            MyLocationData data = new MyLocationData.Builder()
-                    .accuracy(bdLocation.getRadius())
-                    .latitude(bdLocation.getLatitude())
-                    .longitude(bdLocation.getLongitude())
-                    .build();
-            mBaiduMap.setMyLocationData(data);
-            //MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL, )
-
-            LatLng latLng = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
-            if (isFirstIn) {
-                MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
-                mBaiduMap.animateMapStatus(msu);
-                isFirstIn = false;
-            }
-
-            latLngs.add(latLng);
-
-            if (latLngs.size() > 1) {
-                //构建用户绘制多边形的Option对象
-                OverlayOptions polylineOptions = new PolylineOptions().points(latLngs).color(getResources().getColor(R.color.accent_color));
-                //在地图上添加多边形Option，用于显示
-                mBaiduMap.addOverlay(polylineOptions);
-            }
+    @Override
+    public void drawPolyline(ArrayList<LatLng> latLngs) {
+        if (latLngs.size() > 1) {
+            //构建用户绘制多边形的Option对象
+            OverlayOptions polylineOptions = new PolylineOptions().points(latLngs).color(getResources().getColor(R.color.accent_color));
+            //在地图上添加多边形Option，用于显示
+            mBaiduMap.addOverlay(polylineOptions);
         }
+    }
+
+    @Override
+    public void moveCamera2Location(LatLng latLng) {
+        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+        // 这里不能用动画切换镜头，停留在天安门的时间会变长，体验不好，但是现在也还是会闪屏一下……
+        mBaiduMap.setMapStatus(msu);
+    }
+
+    @Override
+    public BaiduMap getBaiduMap() {
+        return mBaiduMap;
     }
 }
