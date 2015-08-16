a7aad01af5c989c484df6eb80d9b88f38b2c70e3
moiling <super8moi@gmail.com>
2015-08-16 08:09:57 +0800

    修一空针，改一图标

--- app/src/main/java/team/far/footing/presenter/WalkPresenter.java
+++ app/src/main/java/team/far/footing/presenter/WalkPresenter.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
-import android.util.Log;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -23,7 +22,6 @@ import team.far.footing.model.bean.MapBean;
 import team.far.footing.service.MapService;
 import team.far.footing.ui.vu.IWalkVu;
 import team.far.footing.util.LogUtils;
-import team.far.footing.util.StringUntils;
 
 /**
  * Created by moi on 2015/8/10.
@@ -172,47 +170,46 @@ public class WalkPresenter {
                     .longitude(bdLocation.getLongitude())
                     .build();
 
-            if (v != null)
+            // 在view没有刷出来的时候、什么都不要做
+            if (v != null) {
                 v.getBaiduMap().setMyLocationData(data);
-            //MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL, )
+                //MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL, )
 
-            LatLng latLng = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
-            // 第一次定位把镜头移向用户当前位置
-            if (isFirstIn) {
-                v.moveCamera2Location(latLng);
-                isFirstIn = false;
-            }
-
-            if (isWalking) {
-                // 先不管怎么样，加入第一个点再说
-                if (latLngs.isEmpty()) latLngs.add(latLng);
-                // 先从距离和加速度两方面控制某一点是否添加到数组中
-                // 还是有些问题，如果第二点是跳点的话没法判断加速度把它删去，所以要先开启定位再开启绘制工作
-                // 当前距离
-                double secondDistance = 0;
-                if (latLngs.size() > 0) {
-                    secondDistance = DistanceUtil
-                            .getDistance(latLng, latLngs.get(latLngs.size() - 1));
+                LatLng latLng = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+                // 第一次定位把镜头移向用户当前位置
+                if (isFirstIn) {
+                    v.moveCamera2Location(latLng);
+                    isFirstIn = false;
                 }
-                // 距离大于10
-                if (secondDistance >= 10) {
-                    if (firstDistance == 0) {
-                        latLngs.add(latLng);
-                        if (v != null)
-                            v.drawPolyline(latLngs);
-                        firstDistance = secondDistance;
-                    } else {
-                        // 加速度
-                        acceleration = (secondDistance - firstDistance) / (span * span);
-                        // 加速度小于10
-                        if (acceleration <= 10) {
+
+                if (isWalking) {
+                    // 先不管怎么样，加入第一个点再说
+                    if (latLngs.isEmpty()) latLngs.add(latLng);
+                    // 先从距离和加速度两方面控制某一点是否添加到数组中
+                    // 还是有些问题，如果第二点是跳点的话没法判断加速度把它删去，所以要先开启定位再开启绘制工作
+                    // 当前距离
+                    double secondDistance = 0;
+                    if (latLngs.size() > 0) {
+                        secondDistance = DistanceUtil
+                                .getDistance(latLng, latLngs.get(latLngs.size() - 1));
+                    }
+                    // 距离大于10
+                    if (secondDistance >= 10) {
+                        if (firstDistance == 0) {
                             latLngs.add(latLng);
-                            if (v != null)
-                                v.drawPolyline(latLngs);
+                            v.drawPolyline(latLngs);
                             firstDistance = secondDistance;
-                            distanceTotal += secondDistance;
-                            if (v != null)
+                        } else {
+                            // 加速度
+                            acceleration = (secondDistance - firstDistance) / (span * span);
+                            // 加速度小于10
+                            if (acceleration <= 10) {
+                                latLngs.add(latLng);
+                                v.drawPolyline(latLngs);
+                                firstDistance = secondDistance;
+                                distanceTotal += secondDistance;
                                 v.showDistanceTotal(distanceTotal);
+                            }
                         }
                     }
                 }
