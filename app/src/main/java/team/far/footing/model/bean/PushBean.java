0ab7a2c0c8c0967142e0a98ed2cacfa3c8140f71
luoyy <1070753575@qq.com>
2015-08-22 18:08:34 +0800

    完善推送  可以直接推送更新  点击就下载。

---
+++ app/src/main/java/team/far/footing/model/bean/PushBean.java
@@ -0,0 +1,31 @@
+package team.far.footing.model.bean;
+
+/**
+ * Created by luoyy on 2015/8/22 0022.
+ */
+public class PushBean {
+
+    String msg;
+
+    String version;
+    String url;
+
+    public String getMsg() {
+        return msg;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
